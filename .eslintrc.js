b62fe2994b964b8916172a6d878d92c628385f24
Greg O'Byrne <greg.obyrne@live.com>
2022-12-08 14:23:07 -0800

    First Upload to the project repository at https://github.com/gregoftheweb/DogsIveMet

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+
+  rules: {
+    // enable additional rules
+    'prettier/prettier': ['error', {endOfLine: 'auto'}],
+  },
+};
---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <HomeScreen />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+});
+
+export default App;
