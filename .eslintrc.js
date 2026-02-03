26741fb71a9bea576261653d6486a29a34add5c4
Greg O <greg.obyrne@live.com>
2026-02-03 11:03:59 -0800

    Rebuild as new Expo App

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: '@react-native-community',
-
-  rules: {
-    // enable additional rules
-    'prettier/prettier': ['error', {endOfLine: 'auto'}],
-  },
-};
--- jest/setup.js
+++
@@ -1,9 +0,0 @@
- 
-// include this line for mocking react-native-gesture-handler
-import 'react-native-gesture-handler/jestSetup';
-
-jest.mock('@react-navigation/native', () => ({
-    useNavigation: () => ({ 
-      navigate: jest.fn()
-    }),
-  }))
